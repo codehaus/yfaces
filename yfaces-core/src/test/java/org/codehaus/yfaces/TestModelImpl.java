5f0453872ef025b70f8e1310237324fb6b37dcc1
dstrietz <dstrietz@5e2dd871-cf5f-0410-99d1-ce23be8b8ae2>
2010-05-10 18:27:14 +0000

    some renamings from 'frame' occurences to 'container'

--- yfaces-core/src/test/java/org/codehaus/yfaces/TestModelImpl.java
+++ yfaces-core/src/test/java/org/codehaus/yfaces/TestModelImpl.java
@@ -1,10 +1,12 @@
 package org.codehaus.yfaces;
 
 import org.codehaus.yfaces.component.AbstractYModel;
+import org.junit.Ignore;
 
+@Ignore
 public class TestModelImpl extends AbstractYModel implements TestModel {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;
 
 	public void testDummy() {
 
